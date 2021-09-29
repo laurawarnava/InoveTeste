b88961b3d796dca4ea95ef1bca29b6d2e8bda8d4
laurawarnava <laurawwarnava@gmail.com>
2021-09-29 18:55:37 -0300

    Add project files.

---
+++ InoveTeste/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InoveTeste")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InoveTeste")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c438f6c9-cdf6-4b54-b5a3-10c2f6e64423")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Parallelizable(ParallelScope.Fixtures)]
